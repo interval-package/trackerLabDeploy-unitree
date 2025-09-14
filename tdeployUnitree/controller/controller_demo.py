c02ee31aaedd759928e3e8aab447767cc84686a6
Zaterval <ziang_zheng@foxmail.com>
2025-09-14 14:49:46 +0800

    add more controllers

---
+++ tdeployUnitree/controller/controller_demo.py
@@ -0,0 +1,106 @@
+from typing import Union
+import numpy as np
+import time
+import torch
+
+from tdeployUnitree.common.rotation_helper import get_gravity_orientation, transform_imu_data
+
+from .controller_base import Controller
+
+
+class ControllerDemo(Controller):
+
+    def init_policy(self):
+        # Initialize the policy network
+        self.policy = torch.jit.load(self.config.policy_path)
+
+    def init_variables(self):
+        # Initializing process variables
+        self.qj             = np.zeros(self.config.num_actions, dtype=np.float32)
+        self.dqj            = np.zeros(self.config.num_actions, dtype=np.float32)
+        self.action         = np.zeros(self.config.num_actions, dtype=np.float32)
+        self.target_dof_pos = self.config.default_angles.copy()
+        self.obs            = np.zeros(self.config.num_obs, dtype=np.float32)
+        self.cmd            = np.array([0.0, 0.0, 0.0])
+        self.counter        = 0
+        
+    def prepare_variables(self):
+        # Get the current joint position and velocity
+        for i in range(len(self.config.leg_joint2motor_idx)):
+            self.qj[i] = self.low_state.motor_state[self.config.leg_joint2motor_idx[i]].q
+            self.dqj[i] = self.low_state.motor_state[self.config.leg_joint2motor_idx[i]].dq
+        
+        self.cmd[0] = self.remote_controller.ly
+        self.cmd[1] = self.remote_controller.lx * -1
+        self.cmd[2] = self.remote_controller.rx * -1
+
+    def construct_obs(self):
+        # imu_state quaternion: w, x, y, z
+        quat = self.low_state.imu_state.quaternion
+        ang_vel = np.array([self.low_state.imu_state.gyroscope], dtype=np.float32)
+
+        if self.config.imu_type == "torso":
+            # h1 and h1_2 imu is on the torso
+            # imu data needs to be transformed to the pelvis frame
+            waist_yaw = self.low_state.motor_state[self.config.arm_waist_joint2motor_idx[0]].q
+            waist_yaw_omega = self.low_state.motor_state[self.config.arm_waist_joint2motor_idx[0]].dq
+            quat, ang_vel = transform_imu_data(waist_yaw=waist_yaw, waist_yaw_omega=waist_yaw_omega, imu_quat=quat, imu_omega=ang_vel)
+
+        # create observation
+        gravity_orientation = get_gravity_orientation(quat)
+        qj_obs = self.qj.copy()
+        dqj_obs = self.dqj.copy()
+        qj_obs = (qj_obs - self.config.default_angles) * self.config.dof_pos_scale
+        dqj_obs = dqj_obs * self.config.dof_vel_scale
+        ang_vel = ang_vel * self.config.ang_vel_scale
+        period = 0.8
+        count = self.counter * self.config.control_dt
+        phase = count % period / period
+        sin_phase = np.sin(2 * np.pi * phase)
+        cos_phase = np.cos(2 * np.pi * phase)
+
+        num_actions = self.config.num_actions
+        self.obs[:3] = ang_vel
+        self.obs[3:6] = gravity_orientation
+        self.obs[6:9] = self.cmd * self.config.cmd_scale * self.config.max_cmd
+        self.obs[9 : 9 + num_actions] = qj_obs
+        self.obs[9 + num_actions : 9 + num_actions * 2] = dqj_obs
+        self.obs[9 + num_actions * 2 : 9 + num_actions * 3] = self.action
+        self.obs[9 + num_actions * 3] = sin_phase
+        self.obs[9 + num_actions * 3 + 1] = cos_phase
+
+    def run(self):
+        self.counter += 1
+        
+        self.prepare_variables()
+        self.construct_obs()
+
+        # Get the action from the policy network
+        obs_tensor = torch.from_numpy(self.obs).unsqueeze(0)
+        self.action = self.policy(obs_tensor).detach().numpy().squeeze()
+        
+        # transform action to target_dof_pos
+        target_dof_pos = self.config.default_angles + self.action * self.config.action_scale
+
+        # Build low cmd
+        for i in range(len(self.config.leg_joint2motor_idx)):
+            motor_idx = self.config.leg_joint2motor_idx[i]
+            self.low_cmd.motor_cmd[motor_idx].q = target_dof_pos[i]
+            self.low_cmd.motor_cmd[motor_idx].qd = 0
+            self.low_cmd.motor_cmd[motor_idx].kp = self.config.kps[i]
+            self.low_cmd.motor_cmd[motor_idx].kd = self.config.kds[i]
+            self.low_cmd.motor_cmd[motor_idx].tau = 0
+
+        for i in range(len(self.config.arm_waist_joint2motor_idx)):
+            motor_idx = self.config.arm_waist_joint2motor_idx[i]
+            self.low_cmd.motor_cmd[motor_idx].q = self.config.arm_waist_target[i]
+            self.low_cmd.motor_cmd[motor_idx].qd = 0
+            self.low_cmd.motor_cmd[motor_idx].kp = self.config.arm_waist_kps[i]
+            self.low_cmd.motor_cmd[motor_idx].kd = self.config.arm_waist_kds[i]
+            self.low_cmd.motor_cmd[motor_idx].tau = 0
+
+        # send the command
+        self.send_cmd(self.low_cmd)
+
+        time.sleep(self.config.control_dt)
+        
